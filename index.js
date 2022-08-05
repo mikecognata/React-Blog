0e4daba9083078c1dce2dbe2b28d08acb6cdf4cc
neokage <66198267+mikecognata@users.noreply.github.com>
2022-08-05 19:44:50 -0400

    added private information to js

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const express  = require("express");
-const app      = express();
-const mongoose = require("mongoose");
+const express      = require("express");
+const app          = express();
+const mongoose     = require("mongoose");
+const bodyParser   = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-mongoose.connect
-("mongodb+srv://hoshikira:pMhFEkFOXildDMU3@react-blog.r21ox5j.mongodb.net/?retryWrites=true&w=majority", 
+const config = require("./config/keys")
+
+const { User } = require("./models/user");
+
+
+mongoose.connect(config.mongoURI, 
 {useNewUrlParser: true }).then(() => console.log("Zion is connected."))
                          .catch(err => console.error(err));
 
 
 
-                         app.get('/', function (req, res) {
-    res.send('Welcome home, neokage.')
-});
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.get("/", (req, res) => {
 
+    res.json({"hello~": "Hit ~~ djhfslkjfhgfhj"})
+})
+
+app.post("/api/users/register", (req, res) => {
+        const user = new User(req.body)
+
+        user.save((err, userData) => {
+            if(err) return res.json ({ sucess: false, err})
+            return res.status(200)
+        })
+
+})
 
 
 app.listen(9000);
