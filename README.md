fd93aced7baf7b2bd370c3572a92f6215c895c27
neokage <66198267+mikecognata@users.noreply.github.com>
2022-08-05 21:53:38 -0400

    auth tokens and postman

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 Today, I worked on ...
 - Practicing building React boilerplate
-- 
-- 
+- Git SSH
+- Privatize information in js
+- Dev / Prod js
 
 ## 
 Today 
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const mongoose     = require("mongoose");
 const bodyParser   = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+
 const config = require("./config/keys")
 
 const { User } = require("./models/user");
-
+const { auth } = require("./middleware/auth");
 
 mongoose.connect(config.mongoURI, 
 {useNewUrlParser: true }).then(() => console.log("Zion is connected."))
@@ -18,20 +19,71 @@ mongoose.connect(config.mongoURI,
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.get("/", (req, res) => {
 
-    res.json({"hello~": "Hit ~~ djhfslkjfhgfhj"})
+
+
+app.get("/api/user/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id     : req._id,
+        isAuth  : true,
+        email   : req.user.email,
+        name    : req.user.name,
+        lastname: req.user.lastname,
+        role    : req.user.role
+
+    })
 })
+
+
 
 app.post("/api/users/register", (req, res) => {
-        const user = new User(req.body)
+        const user = new User(req.body);
 
-        user.save((err, userData) => {
-            if(err) return res.json ({ sucess: false, err})
-            return res.status(200)
-        })
+        user.save((err, doc) => {
+            if(err) return res.json ({ sucess: false, err});
+            res.status(200).json({
+                success: true,
+                userData: doc
+            });
+        });
+
+});
+
+
+app.post("/api/user/login", (req, res) => {
+    //find email
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user)
+        return res.json({
+            loginSuccess: false,
+            message: "Auth failed, email not found"
+        });
+    
+    
+    //comparePassword
+    
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch){
+            return res.json ({ loginSuccess: false, message: "wrong password"})
+        }     
+    })
+
+    // genereateToke
+
+    user.generateToken((err, user) =>{
+        if(err) return res.status(400).send(err);
+        res.cookie("x_auth", user.token)
+            .status(200)
+            .json({
+                loginSuccess: true
+            })
+    })
+    })
+
+
 
 })
+
 
 
 app.listen(9000);
